964accf12d528bf0218c697c1b6db0ab51c5e473
breader124 <breaderman@gmail.com>
2021-02-28 17:24:52 +0100

    replaced accept header annotation with LocaleContextHolder

--- src/main/java/com/breader/springmicroservices/helloworld/HelloWorldController.java
+++ src/main/java/com/breader/springmicroservices/helloworld/HelloWorldController.java
@@ -2,9 +2,10 @@ package com.breader.springmicroservices.helloworld;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Locale;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,12 +19,12 @@ public class HelloWorldController {
     }
 
     @GetMapping("helloBean/{name}")
-    public @ResponseBody HelloWorldBean helloWorldBean(@PathVariable String name) {
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
     @GetMapping("hello-world-internationalized")
-    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
-        return messageSource.getMessage("good.morning.message", null, locale);
+    public String helloWorldInternationalized() {
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
