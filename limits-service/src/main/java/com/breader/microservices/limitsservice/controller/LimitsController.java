8ad367841aced9ee0801eccec3cc2ab8c52f2b8a
breader124 <breaderman@gmail.com>
2021-03-07 19:32:59 +0100

    improved LimitsService to fetch parameters from configuration files and generated another Spring Boot project which will become Config Server

--- limits-service/src/main/java/com/breader/microservices/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/com/breader/microservices/limitsservice/controller/LimitsController.java
@@ -1,15 +1,20 @@
 package com.breader.microservices.limitsservice.controller;
 
 import com.breader.microservices.limitsservice.bean.Limits;
+import com.breader.microservices.limitsservice.configuration.Configuration;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class LimitsController {
+
+    private final Configuration configuration;
 
     @GetMapping("limits")
     public Limits retrieveLimits() {
-        return new Limits(1, 5);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 
 }
